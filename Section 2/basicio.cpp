9c2a7bce913c703bec8b322a5242287b7854d524
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2022-08-06 23:28:05 +0530

    New Commits 2

---
+++ Section 2/basicio.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int num1,num2;
+    double num3;
+    // cout<<"Hello World!!"<<endl;
+    // cout<<"Hello"<<"World"<<endl;
+    // cout<<"Hello"<<"\nWorld\n";
+    // cout<<"Hello\nOut\nThere\n";
+
+
+    // cout<<"Enter a number: ";
+    // cin>>num1;
+    // cout<<"You entered "<<num1<<endl;
+
+    // cout<<"Enter another number: ";
+    // cin>>num2;
+    // cout<<"You entered "<<num1<<" first then you entered "<<num2;
+
+    // cout<<"Enter two numbers seperated by space: ";
+    // cin>>num1>>num2;
+    // cout<<"You entered "<<num1<<" first then you entered "<<num2;
+    // cout<<"Enter a double: ";
+    // cin>>num3;
+    // cout<<"You entered "<<num3<<endl;
+    cout<<"Enter an integer: ";
+    cin>>num1;
+    
+    
+    
+    
+    
+    return 0;
+
+}
